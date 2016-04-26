28f7a35f17bac324973f2a80bc398251a44ab2b6
Chowdhurian <manil.chowdhury@gmail.com>
2016-04-26 02:55:19 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RottenClementines::Application.config.secret_key_base = '0411a4cdcb3ca8b3348cac86800fb8e35d06bfc589c4710afb8c842c171f6e7d203ff33481f1fd6641cb6e8320fc7fc1b42e43a844a2ecdad479ad6a31e23cb0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RottenClementines::Application.config.session_store :cookie_store, key: '_rotten_clementines_session'
