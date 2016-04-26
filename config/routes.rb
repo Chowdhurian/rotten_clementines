2b7c765ede4104c35bde87299e520281cd5eaed3
Chowdhurian <manil.chowdhury@gmail.com>
2016-04-26 18:00:38 +0000

    migrate name fields to users, has_secure_password

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ RottenClementines::Application.routes.draw do
   resources :materials
 
   root to: 'materials#index'
-  # The priority is based upon order of creation: first created -> highest priority.
+  
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
