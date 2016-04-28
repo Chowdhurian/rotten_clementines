615963246d871f58271ad8def398d56c941ccc7c
Chowdhurian <manil.chowdhury@gmail.com>
2016-04-28 03:23:06 +0000

    delete working

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,11 @@
 class Admin::UsersController < AdminController
   def index
-    @users = User.order(:email).page params[:page]
+    @users = User.order(:email).page(params[:page])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 
   has_secure_password
 
-  validates :email, presence: true #need to validate email format
+  validates :email, presence: true, uniqueness: true, length: { maximum: 100 }, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: 'does not appear to be valid' }
+
   paginates_per 1
 
   def full_name
